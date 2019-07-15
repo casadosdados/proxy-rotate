8740b644b7cb035e70c7f936628da017d8b5945a
Jhonata Menezes <jhonatamenezes10@gmail.com>
2019-07-15 15:53:53 -0300

    set timeout

--- main/proxy.go
+++ main/proxy.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	proxy2 "github.com/casadosdados/proxy-rotate/proxy"
 	"log"
+	"time"
 )
 
 func main() {
@@ -14,6 +15,12 @@ func main() {
 	flag.StringVar(&host, "h", "0.0.0.0:8888", "host and port")
 	flag.Parse()
 
+	httpServer := &http.Server{
+		Addr:         host,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+	httpServer.SetKeepAlivesEnabled(false)
 	proxy2.ProxyList = new(proxy2.ProxyBucket)
 	go proxy2.ProxyList.Start()
 	fmt.Println("Starting proxy server ", host)
@@ -21,5 +28,6 @@ func main() {
 	//proxy.Verbose = true
 	proxy.Tr = proxy2.NewTransport(proxy)
 
-	log.Fatal(http.ListenAndServe(host, proxy))
+	httpServer.Handler = proxy
+	log.Fatal(httpServer.ListenAndServe())
 }
